88692cf02c3688502b9cbbfe78bdd279ea204265
RobertKurwica <itiguuy@gmail.com>
2020-03-11 00:42:34 +0100

    psycha siada

--- csvReaderXML/csvReaderXML/Program.cs
+++ csvReaderXML/csvReaderXML/Program.cs
@@ -12,7 +12,73 @@ namespace csvReaderXML
         static void Main(string[] args)
         {
 
+            System.IO.StreamWriter logsFile = new System.IO.StreamWriter(@"Logs.txt");
+            
 
+            String csvPath;
+            String targetPath;
+            String format;
+
+            if (args.Length == 3)
+            {
+                
+                
+                csvPath = args[0];
+                try {
+                    PathTest(csvPath);
+                }
+                catch(ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+                    csvPath = DEFAULT_SOURCE_PATH;
+                    logsFile.WriteLine("Source file path contains illegal chars -> Path changed to default: " + DEFAULT_SOURCE_PATH);
+                }
+                targetPath = args[1];
+                format = args[2];
+                try
+                {
+                    PathTest(targetPath);
+                }
+                catch(ArgumentException e)
+                {
+
+                    Console.WriteLine(e.Message);
+                    format = DEFAULT_FORMAT;
+                    targetPath = DEFAULT_TARGET_PATH;
+                    logsFile.WriteLine("Target file path contains illegal chars -> Path changed to default: " + DEFAULT_TARGET_PATH);
+                }
+               
+
+            }
+            else
+            {
+
+                csvPath = DEFAULT_SOURCE_PATH;
+                targetPath = DEFAULT_TARGET_PATH;
+                format = DEFAULT_FORMAT;
+
+
+            }
+
+            if (File.Exists(targetPath))
+                File.Delete(targetPath);
+
+
+            logsFile.Close();
+            logsFile.Dispose();
+        }
+
+        static Boolean PathTest(String path)
+        {
+ 
+            char[] invalidChars = Path.GetInvalidPathChars();
+            foreach (char letter in invalidChars)
+            {
+                if (path.Contains(letter))
+                    throw new ArgumentException(String.Format("Argument contains illegal chars."));
+            }
+
+            return true;
 
         }
     }
