50180d1ab04e7736122725e883021e04e4a0613d
RobertKurwica <itiguuy@gmail.com>
2020-03-11 00:31:28 +0100

    bedzie magik

--- csvReaderXML/csvReaderXML/Program.cs
+++ csvReaderXML/csvReaderXML/Program.cs
@@ -12,70 +12,7 @@ namespace csvReaderXML
         static void Main(string[] args)
         {
 
-            System.IO.StreamWriter logsFile = new System.IO.StreamWriter(@"Logs.txt");
-            Console.WriteLine(logsFile);
 
-            String csvPath;
-            String targetPath;
-            String format;
-
-            if (args.Length == 3)
-            {
-                
-                
-                csvPath = args[0];
-                try {
-                    PathTest(csvPath);
-                }
-                catch(ArgumentException e)
-                {
-                    csvPath = DEFAULT_SOURCE_PATH;
-                    logsFile.WriteLine("Source file path contains illegal chars -> Path changed to default: " + DEFAULT_SOURCE_PATH);
-                }
-                targetPath = args[1];
-                format = args[2];
-                try
-                {
-                    PathTest(targetPath);
-                }
-                catch(ArgumentException e)
-                {
-                    format = DEFAULT_FORMAT;
-                    targetPath = DEFAULT_TARGET_PATH;
-                    logsFile.WriteLine("Target file path contains illegal chars -> Path changed to default: " + DEFAULT_TARGET_PATH);
-                }
-               
-
-            }
-            else
-            {
-
-                csvPath = DEFAULT_SOURCE_PATH;
-                targetPath = DEFAULT_TARGET_PATH;
-                format = DEFAULT_FORMAT;
-
-
-            }
-
-            if (File.Exists(targetPath))
-                File.Delete(targetPath);
-
-
-            logsFile.Close();
-            logsFile.Dispose();
-        }
-
-        static Boolean PathTest(String path)
-        {
- 
-            char[] invalidChars = Path.GetInvalidPathChars();
-            foreach (char letter in invalidChars)
-            {
-                if (path.Contains(letter))
-                    throw new ArgumentException(String.Format("Argument contains illegal chars."));
-            }
-
-            return true;
 
         }
     }
